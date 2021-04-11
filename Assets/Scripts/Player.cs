6e8d4d1b0adea54d44f2236e6fc036b6e15f3a8d
Josh <vangj36@gmatc.matc.edu>
2021-04-10 21:40:56 -0500

    Player Created

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private int _playerSpeed = 2;
+
     void Start()
     {
-        transform.position = new Vector3(0, 0, 0);
+        //transform.position = new Vector3(0, 0, 0);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        CalculateMovement();
+
+    }
+
+    void CalculateMovement()
+    {
+        //Variables
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalAxis, verticalAxis, 0);
+
+
+        //Vertical Clamp
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0f), 0f);
+
+        //Horizontal Clamp
+        if (transform.position.x > 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, transform.position.z);
+        }
+
+        //Move the player
+        transform.Translate(direction * Time.deltaTime * _playerSpeed);
     }
 }
